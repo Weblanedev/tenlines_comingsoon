d624e9142e200b906f1156d17859402c14a1fb11
Olu-martins <mayowasamuel@gmail.com>
2024-11-01 16:06:55 +0100

    fix issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-
-import './App.css'
+import "./App.css";
+import underMaintenanceImage from "./assets/under-maintenance.jpg";
 
 function App() {
-
   return (
     <div>
       <div className="coming-soon-image">
         <img
           height={400}
           width={600}
-          src="src/assets/under-maintenance.jpg"
-          alt="img"
+          src={underMaintenanceImage}
+          alt="Under Maintenance"
         />
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
